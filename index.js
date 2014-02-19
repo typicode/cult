095aae9ed8e04c3ebad07fef077485c39ab672ef
Typicode <typicode@gmail.com>
2014-02-19 08:24:48 +0100

    Add LiveScript

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
+var fs = require('fs');
 var spawn = require('child_process').spawn;
+
+var options = {
+  'coffee': 'coffee-script/register',
+  'ls': 'LiveScript'
+};
 
 // Keep only arguments
 process.argv.splice(0, 2);
 
-// Add CoffeeScript option
-process.argv.push('--require');
-process.argv.push('coffee-script/register');
+// If there's no gulpfile.js
+if (!fs.existsSync('gulpfile.js')) {
+  // Look for gulpfile with another extension
+  // and add corresponding --require option to process.argv
+  for (var ext in options) {
+    if (fs.existsSync('gulpfile.'+ext)) {
+      process.argv.push('--require');
+      process.argv.push(options[ext]);
+      break;
+    }
+  }
+}
 
-// Call gulp
+// Then call gulp
 spawn('gulp', process.argv, {stdio: "inherit"});
