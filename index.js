c00134078db03e401a7214505438da5538d139a5
Adam Lynch <contact@adamlynch.ie>
2014-05-17 23:56:14 +0100

    Indentation fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 
 var options = {
-    'coffee': 'coffee-script/register',
-    'ls': 'LiveScript'
+  'coffee': 'coffee-script/register',
+  'ls': 'LiveScript'
 };
 
 // Keep only arguments
@@ -12,33 +12,33 @@ process.argv.splice(0, 2);
 
 // If there's no gulpfile.js
 if (!fs.existsSync('gulpfile.js')) {
-    // Look for gulpfile with another extension
-    // and add corresponding --require option to process.argv
-    for (var ext in options) {
-        if (fs.existsSync('gulpfile.'+ext)) {
-            process.argv.push('--require');
-            process.argv.push(options[ext]);
-            break;
-        }
-    }
+  // Look for gulpfile with another extension
+  // and add corresponding --require option to process.argv
+  for (var ext in options) {
+  if (fs.existsSync('gulpfile.'+ext)) {
+    process.argv.push('--require');
+    process.argv.push(options[ext]);
+    break;
+  }
+  }
 }
 
 // Then call gulp
 
 var callSpawn = function(useCmd){
-    return spawn('gulp' + (useCmd ? '.cmd' : ''), process.argv, {
-        stdio: 'inherit'
-    });
+  return spawn('gulp' + (useCmd ? '.cmd' : ''), process.argv, {
+  stdio: 'inherit'
+  });
 };
 
 callSpawn(false).on('error', function(error){
-    var platform = require('os').platform();
+  var platform = require('os').platform();
 
-    // if gulp isn't found on Windows, try gulp.cmd
-    if(error.code === 'ENOENT' && (platform === 'win32' || platform === 'win64')){
-        callSpawn(true);
-    }
-    else {
-        throw error;
-    }
+  // if gulp isn't found on Windows, try gulp.cmd
+  if(error.code === 'ENOENT' && (platform === 'win32' || platform === 'win64')){
+  callSpawn(true);
+  }
+  else {
+    throw error;
+  }
 });
