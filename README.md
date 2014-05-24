d780116fbbd99d8e46a1f92df8a768df20761ebb
Typicode <typicode@gmail.com>
2014-05-24 02:03:14 +0200

    Add watch

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
-# cult [![NPM version](https://badge.fury.io/js/cult.png)](http://badge.fury.io/js/cult)
+# cult [![NPM version](https://badge.fury.io/js/cult.svg)](http://badge.fury.io/js/cult)
 
-cult is a no-brainer tool that detects `gulpfile` extension and calls your local `gulp` with the corresponding `--require` option.
+![cult](http://i.imgur.com/aHMew7e.png)
 
-## Usage
+> cult is to gulp what nodemon is to node.
 
-Just use cult as you use gulp.
+# Features
 
+* Detects gulpfile extension and calls gulp accordingly 
+* Watch and reload gulp when gulpfile changes
+* Supports gulpfile written in JavaScript, CoffeeScript, CoffeeScriptRedux and LiveScript.
+
+# Usage
+
+Install everything the same way you would do with gulp.
 ```bash
-$ cult <task> <othertask>
+$ npm install -g gulp cult 
+$ npm install --save-dev coffee-script gulp
 ```
 
-Supports `gulpfile` written in JavaScript, CoffeeScript and LiveScript.
-
-| command       | file            | will call
-|:--------------|:----------------|:--------------------------------------------
-| cult \<task\> | gulpfile.js     | gulp \<task\>
-| cult \<task\> | gulpfile.coffee | gulp \<task\> --require coffee-script/register
-| cult \<task\> | gulpfile.ls     | gulp \<task\> --require LiveScript
-
-## Install
-
+Create your gulpfile in your favourite language.
 ```bash
-$ npm install -g gulp cult
+$ vim gulpfile.coffee
+$ cult -w <task> <othertask>
 ```
 
-## Contribute
+It will call `gulp --require coffee-script/register` and reload it whenever gulpfile.coffee changes.
+
+If you don't want cult to watch your gruntfile, just remove the `-w` parameter.
+
+# Contribute
 
 Missing a language? Fork and create a pull request.
 
@@ -35,5 +39,3 @@ Missing a language? Fork and create a pull request.
 ## License
 
 MIT
-
-
--- index.js
+++ index.js
@@ -1,50 +1,79 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var spawn = require('child_process').spawn;
+var fs         = require('fs')
+var platform   = require('os').platform()
+var spawn      = require('child_process').spawn
+var chalk      = require('chalk')
+var FILES = {}
 
-// Detecting coffee-script-redux before coffee-script
+FILES['gulpfile.js'] = undefined
+
 try {
-  require.resolve('coffee-script-redux');
-  var coffeescript = 'coffee-script-redux';
+  require.resolve('coffee-script-redux')
+  FILES['gulpfile.coffee'] = 'coffee-script-redux/register'
 } catch(e) {
-  var coffeescript = 'coffee-script';
+  FILES['gulpfile.coffee'] = 'coffee-script/register'
+} 
+
+FILES['gulpfile.ls'] = 'LiveScript'
+
+// Keep only gulp arguments
+process.argv.splice(0, 2)
+
+// And remove cult specific arguments
+var watch = false
+var watchIndex = process.argv.indexOf('-w')
+if (watchIndex > -1) {
+  process.argv.splice(watchIndex, 1)
+  watch = true
 }
 
-var options = {
-  'coffee': coffeescript + '/register',
-  'ls': 'LiveScript'
-};
+// Find gulpfile
+var gulpfileName
+for (var file in FILES) {
+  if (fs.existsSync(file)) {
+    gulpfileName = file
+    break;
+  }
+}
 
-// Keep only arguments
-process.argv.splice(0, 2);
+// Add --require
+var requireOption = FILES[gulpfileName]
+if (requireOption) {
+  process.argv.push('--require')
+  process.argv.push(requireOption)
+}
 
-// If there's no gulpfile.js
-if (!fs.existsSync('gulpfile.js')) {
-  // Look for gulpfile with another extension
-  // and add corresponding --require option to process.argv
-  for (var ext in options) {
-    if (fs.existsSync('gulpfile.'+ext)) {
-      process.argv.push('--require');
-      process.argv.push(options[ext]);
-      break;
+// Find gulpfile
+var gulpfileName
+for (var file in FILES) {
+  if (fs.existsSync(file)) {
+    gulpfileName = file
+    break;
+  }
+}
+
+// Let's go
+function log(str) {
+  console.log('[' + chalk.cyan('cult') + '] ' + str)
+}
+
+function run() {
+  var child = spawn('gulp', process.argv, { stdio: 'inherit' })
+  child.on('error', function(error) {
+    child.kill()
+    if (error.code === 'ENOENT' && (platform === 'win32' || platform === 'win64')) {
+      spawn('gulp.cmd', process.argv, { stdio: 'inherit' })
     }
-  }
+  })
 }
 
-// Then call gulp
-var callSpawn = function(useCmd){
-  return spawn('gulp' + (useCmd ? '.cmd' : ''), process.argv, {
-    stdio: 'inherit'
-  });
-};
+if (watch) {
+  log('Watching ' + gulpfileName)
+  fs.watchFile(gulpfileName, function() {
+    console.log()
+    log('Reloading')
+    run()
+  })
+}
 
-callSpawn(false).on('error', function(error){
-  var platform = require('os').platform();
-
-  // if gulp isn't found on Windows, try gulp.cmd
-  if(error.code === 'ENOENT' && (platform === 'win32' || platform === 'win64')){
-    callSpawn(true);
-  } else {
-    throw error;
-  }
-});
+run()
