a6c208d48f133a286f5723a1a87bd9186345dc1e
Typicode <typicode@gmail.com>
2014-12-15 04:09:54 +0100

    Rewrite and fix exit on SIGINT

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-#!/usr/bin/env node
-var fs             = require('fs')
-var platform       = require('os').platform()
-var spawn          = require('child_process').spawn
+var os = require('os')
+var fs = require('fs')
+var respawn = require('respawn')
+var chalk = require('chalk')
 var updateNotifier = require('update-notifier')
-var chalk          = require('chalk')
+var pkg = require('./package.json')
 
-// Notifier
-var notifier = updateNotifier()
-if (notifier.update) notifier.notify()
+/*
+* Functions
+*/
 
-// Keep gulp arguments
-process.argv.splice(0, 2)
-
-// Find gulpfile
 function findGulpfile() {
   var filenames = fs.readdirSync(process.cwd())
   for (var i in filenames) {
@@ -24,51 +20,55 @@ function log(str) {
   console.log('[' + chalk.cyan('cult') + '] ' + str)
 }
 
-var child
-function run() {
-  log('Running gulp...')
-  child = spawn('gulp', process.argv, { stdio: 'inherit' })
-  child.on('error', function(error) {
-    child.kill('SIGKILL')
-    if (error.code === 'ENOENT' && (platform === 'win32' || platform === 'win64')) {
-      spawn('gulp.cmd', process.argv, { stdio: 'inherit' })
-    }
-  })
-  child.on('exit', function() {
-    // Exiting when gulp finishes all tasks
-    process.exit()
-  })
-  return child
-}
+/*
+* Program
+*/
 
-function stop(cb) {
-  if (!child) return cb()
+// Check for updates
+updateNotifier({packageName: pkg.name, packageVersion: pkg.version}).notify()
 
-  child.removeAllListeners('exit')  // Prevent exiting on reload
-  child.on('exit', cb)
+// Keep gulp arguments
+process.argv.splice(0, 2)
 
-  log('Killing gulp...')
-  child.kill('SIGINT')
-}
-
+// Find gulpfile
 var gulpfile = findGulpfile()
 
+// Set process options
+var options = {
+  kill: 5000, // kill after 5 seconds
+  stdio: 'inherit'
+}
+
+// Create process monitor
+var monitor = respawn(['gulp'].concat(process.argv), options)
+monitor.maxRestarts = 0
+
+// If on windows and gulp fails, try to replace it with gulp.cmd
+monitor.on('exit', function(code, signal) {
+  if (code === 'ENOENT' && (os.platform === 'win32' || os.platform === 'win64')) {
+    monitor = respawn(['gulp.cmd'].concat(process.argv), options)
+    monitor.start()
+  }
+})
+
+// Start cult
 if (gulpfile) {
-  log('Watching ' + gulpfile)
+  log('Watching ' + chalk.magenta(gulpfile))
 
   fs.watchFile(gulpfile, function() {
-    log('Gulpfile changed. Reloading...')
-    stop(function() {
-      run()
+    log('Gulpfile changed, reloading...')
+    monitor.stop(function() {
+      monitor.start()
     })
   })
 
-  run()
-
   process.on('SIGINT', function() {
-    // Prevent killing the main process before child
-    if (!child) process.exit()
+    monitor.stop(function() {
+      process.exit()
+    })
   })
+
+  monitor.start()
 } else {
   return log('Can\'t find gulpfile')
 }
